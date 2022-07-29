56f9cf4fc33a8a97c68cfc457888d0a96ca6ed87
OlegYurov <96524921+OlegYurov@users.noreply.github.com>
2022-07-29 17:34:23 +0300

    add localStorage

---
+++ my-app/src/components/Form/Form.jsx
@@ -0,0 +1,54 @@
+import  React, { Component } from 'react';
+import FormBtn from '../FormBtn/FormBtn'
+import Input from '../Input/Input'
+import { v4 as uuidv4 } from 'uuid';
+import css from './Form.module.css';
+import PropTypes from 'prop-types';
+
+
+class Form extends Component {
+  state = { 
+    name: '',
+    number: '',
+  }
+  
+    setForm = e => {
+          const { value, name } = e.target;
+        this.setState({ [name]: value})
+  }
+  
+      submitForm = e => {
+        e.preventDefault();
+       
+        const createContact = {
+          name: this.state.name,
+          number: this.state.number,
+          id: uuidv4(),
+        };
+        this.props.addContact(createContact);
+        this.setState({ 
+    name: '',
+    number: '',
+  })
+        
+           };
+
+  render() { 
+    
+    return ( 
+       <form className={css.form} onSubmit={this.submitForm}>
+        <Input contact={this.state.number} nameUser={this.state.name} handleInputValue={this.setForm} />
+        <FormBtn textBtn={"Add contact"} />
+          
+      </form>
+      
+     );
+  }
+}
+
+Form.propTypes = {
+  addContact: PropTypes.func.isRequired,
+}
+ 
+export default Form;
+
