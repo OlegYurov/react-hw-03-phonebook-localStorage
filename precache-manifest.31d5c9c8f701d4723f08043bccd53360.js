83298a522a39115166b08da5a5707936f9437fcc
OlegYurov <96524921+OlegYurov@users.noreply.github.com>
2022-07-29 17:39:43 +0300

    Updates

---
+++ precache-manifest.31d5c9c8f701d4723f08043bccd53360.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b822ce7048a2004a21ed",
+    "url": "/react-hw-03-phonebook-localStorage/static/css/main.5875c1c0.chunk.css"
+  },
+  {
+    "revision": "b822ce7048a2004a21ed",
+    "url": "/react-hw-03-phonebook-localStorage/static/js/main.b822ce70.chunk.js"
+  },
+  {
+    "revision": "c57be8183b9577897f2f",
+    "url": "/react-hw-03-phonebook-localStorage/static/js/1.c57be818.chunk.js"
+  },
+  {
+    "revision": "8227236e7c7171d60551",
+    "url": "/react-hw-03-phonebook-localStorage/static/js/runtime~main.8227236e.js"
+  },
+  {
+    "revision": "aa580ed07bc882cff75e7ba26def2264",
+    "url": "/react-hw-03-phonebook-localStorage/index.html"
+  }
+];
